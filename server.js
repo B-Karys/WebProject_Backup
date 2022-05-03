e44f90eb25a60b073d4d0af884e44ad5a51fc3c5
Kerupie <97594360+Kerupie@users.noreply.github.com>
2022-05-04 00:38:23 +0600

    adding the database

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 const express = require('express'),
     router = express.Router()
 const app = express();
-const path = require('path');
-const port = 3002;
+const path = require("path");
+const port = 3001;
+const mongoose = require('mongoose');
+const config = require('config');
+
+const dbConfig = config.get('Kelphy.dbConfig.dbName');
+
+mongoose.connect(dbConfig,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }).then(()=>{
+    console.log('Database Connected');
+}).catch(err=>{
+    console.log('Database not connected'+err)
+});
 
 app.set('views', './views')
 app.set('view engine', 'ejs')
 
 app.use(express.static(__dirname + '/public'))
+
+
 router.get('', (req, res) => {
     res.render('', {text: "About the page"})
 })
