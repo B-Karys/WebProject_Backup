dad9633dc8e4d50f31d9310aa63a1882259c9254
Kerupie <97594360+Kerupie@users.noreply.github.com>
2022-05-10 16:24:21 +0600

    Deleted router, nothing is bad Added procfile,gitignor fixed nav

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const port = process.env.PORT || 3002;
 const bodyParser = require('body-parser');
 const dbConfig = require('./config/database.config.js');
 const mongoose = require('mongoose');
-const router = express.Router();
 //DataBase//
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
@@ -29,9 +28,7 @@ app.set('views', './views')
 app.set('view engine', 'ejs')
 
 app.use(express.static(__dirname + '/public'))
-router.get('routes', (req, res) => {
-    res.render('', {text: "About the page"})
-})
+
 
 //ROOTS//
 app.use('/', require("./routes/index"));
